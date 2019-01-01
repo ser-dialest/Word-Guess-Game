96d27747f3c5c68130f8a92f80bd71b321126bc7
Dialest <dialest@hotmail.com>
2018-12-31 20:28:53 -0800

    Linked files and started game

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+
+// Establish a list of possible words
+var foods = ["Coffee", "Tea", "Hot Cocoa", "Pancakes", "Waffles"];
+
+// Select a random word from list
+var word = foods[Math.floor(Math.random()*foods.length)];
+console.log(word);
+
