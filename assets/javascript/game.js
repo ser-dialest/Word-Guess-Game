9119375f2a1254c850e7f229afbd8625150f2eab
Dialest <dialest@hotmail.com>
2019-01-01 01:32:14 -0800

    Create and display array of blanks for the word that will be guessed.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,5 +4,19 @@ var foods = ["Coffee", "Tea", "Hot Cocoa", "Pancakes", "Waffles"];
 
 // Select a random word from list
 var word = foods[Math.floor(Math.random()*foods.length)];
-console.log(word);
+console.log(word); //Checking
 
+//Create blanks for random word
+var blanks = [];
+for (l = 0; l < word.length; l ++) {
+    if (word[l] === " ") {
+        blanks.push(" ");
+    }
+    else {
+        blanks.push("_");
+    }
+}
+
+// Display the number of blanks for the wor to be guessed
+console.log(blanks.join(" ")); //Checking
+document.getElementById("answer").innerHTML = blanks.join(" ");
